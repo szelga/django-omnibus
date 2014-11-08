67f96e5acd9912159bf41cfc4abacb460de2f1d4
Wasil W Siargiejczyk <szelga.wws@gmail.com>
2014-11-08 20:35:00 +0500

    bind to UNIX socket if OMNIBUS_SERVER_PORT is None

--- omnibus/management/commands/omnibusd.py
+++ omnibus/management/commands/omnibusd.py
@@ -3,11 +3,13 @@ import logging
 from django.core.management.base import BaseCommand
 from django.utils.module_loading import import_by_path
 from tornado import ioloop
+from tornado.httpserver import HTTPServer
+from tornado.netutil import bind_unix_socket
 
 from ...pubsub import PubSub
 from ...settings import (
     SERVER_PORT, AUTHENTICATOR_FACTORY, CONNECTION_FACTORY, WEBAPP_FACTORY,
-    DIRECTOR_ENABLED, FORWARDER_ENABLED)
+    DIRECTOR_ENABLED, FORWARDER_ENABLED, SERVER_HOST)
 
 
 logger = logging.getLogger(__name__)
@@ -31,9 +33,19 @@ class Command(BaseCommand):
         connection_factory = import_by_path(CONNECTION_FACTORY)
         webapp_factory = import_by_path(WEBAPP_FACTORY)
 
-        # Create app and listen on SEVER_PORT
+        # Create app
         app = webapp_factory(connection_factory(authenticator_factory(), pubsub))
-        app.listen(SERVER_PORT)
+        if SERVER_PORT is None:
+            # Listen on UNIX socket
+            server = HTTPServer(app)
+            socket = bind_unix_socket(SERVER_HOST)
+            server.add_socket(socket)
+        else:
+            # Listen on SERVER_HOST:SERVER_PORT
+            if SERVER_HOST:
+                app.listen(SERVER_PORT, address=SERVER_HOST)
+            else:
+                app.listen(SERVER_PORT)
 
         loop = ioloop.IOLoop().instance()
         try:
